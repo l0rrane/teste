04d5182a56497713e8618266cf41194ba51a266c
l0rrane <gabrielalorraneespin@gmail.com>
2026-03-06 09:22:53 -0300

    Atualização index

---
+++ index.js
@@ -0,0 +1,26 @@
+const express =  require("express")
+const app = express()
+
+app.get("/:par", (req, res)=>{
+    const {par} = req.params
+    res.status(200).json({
+        mensagem:`Seu nome é: ${par}`
+    })
+    
+})
+
+app.get("/idade/:nome/:idade", (req, res)=>{
+    const {nome, idade} = req.params
+    res.status(200).json({
+        mensagem:`Seu nome é: ${nome}, Sua idade é: ${idade}`
+    })
+})
+
+
+app.get("/teste", (req, res)=>{
+    res.send("Olá Mundo, test")
+})
+
+app.listen(3011,  ()=>{
+    console.log(" Gabriela o Servidor esta ligado")
+})
